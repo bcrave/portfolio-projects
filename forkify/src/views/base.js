22e038a726c0eac3f8990ed08553299f2e47b7f1
bcrave <brennycode@gmail.com>
2020-05-17 19:58:19 +0200

    added loading animation

--- forkify/src/views/base.js
+++ forkify/src/views/base.js
@@ -1,5 +1,28 @@
 export const elements = {
   searchForm: document.querySelector(".search"),
   searchInput: document.querySelector(".search__field"),
+  searchRes: document.querySelector(".results"),
   searchResList: document.querySelector(".results__list"),
+};
+
+export const elementStrings = {
+  loader: "loader",
+};
+
+export const renderLoader = (parent) => {
+  const loader = `
+    <div class="${elementStrings.loader}">
+        <svg>
+            <use href="../../static/assets/img/loader.svg"></use> 
+        </svg>
+    </div>
+  `; //#icon-cw --possible add this to the end of loader.svg?
+  parent.insertAdjacentHTML("afterbegin", loader);
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) {
+    loader.parentElement.removeChild(loader);
+  }
 };
